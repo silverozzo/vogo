28cc30f7d9537af563f145051862ecb4f57d2285
silverozzo <silverozzo@gmail.com>
2014-12-15 16:24:40 +0600

    forgot change path for static images in config

--- guts/config.py
+++ guts/config.py
@@ -1,4 +1,4 @@
 class Config:
 	draw_center = 34
-	static_unit_dir = "D:\\own\\vogo\\static_symbols\\"
-	mark_figure_dir = "D:\\own\\vogo\\static_symbols\\marks\\"
+	static_unit_dir = "static_symbols\\"
+	mark_figure_dir = "static_symbols\\marks\\"
